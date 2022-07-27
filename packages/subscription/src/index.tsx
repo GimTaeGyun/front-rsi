ec6c16a70946931daaa5458e5af875375ee53d50
James <james@g9soft.com>
2022-07-27 15:22:10 +0900

    index Route

--- packages/subscription/src/index.tsx
+++ packages/subscription/src/index.tsx
@@ -1,7 +1,7 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { Provider } from 'jotai';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import AddGroup from './components/Admin/AddGroup';
@@ -10,14 +10,16 @@ import AppFrame from './container/AppFrame';
 import AdminLogin from './container/Login';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.render(<AppFrame />, document.getElementById('root'));
-
 export { default as Header } from './Header';
 export { default as Login } from './container/Login';
 export { default as ClientArea } from './components/ClientArea';
 export { default as AppFrame } from './container/AppFrame';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
   <React.StrictMode>
     <Provider>
       <React.Suspense fallback={<div>Loading...</div>}>
@@ -36,7 +38,6 @@ ReactDOM.render(
       </React.Suspense>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
