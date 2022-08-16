a92f98f8f55de988aea3941819a9bc013a7c7eaf
James <james@g9soft.com>
2022-08-16 10:59:53 +0900

    no message

--- packages/subscription/src/components/MenuCCP/customer/components/Info2.tsx
+++ packages/subscription/src/components/MenuCCP/customer/components/Info2.tsx
@@ -36,11 +36,7 @@ const Info = () => {
   return (
     <>
       <Card className="sub_ccp_detail_parent">
-        <CardHeader
-          disableTypography
-          title="법인 담당자 정보"
-          className="header"
-        />
+        <CardHeader disableTypography title="개인 정보" className="header" />
         <Divider />
         <CardContent className="content" sx={{ padding: '0px' }}>
           <Box className="column">
