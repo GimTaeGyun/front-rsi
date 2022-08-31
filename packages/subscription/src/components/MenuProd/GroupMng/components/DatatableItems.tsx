be940ef0772f91f3483f035bb91ae974c9d98b82
James <james@g9soft.com>
2022-08-31 14:58:27 +0900

    datatable footer 따로뺌

--- packages/subscription/src/components/MenuProd/GroupMng/components/DatatableItems.tsx
+++ packages/subscription/src/components/MenuProd/GroupMng/components/DatatableItems.tsx
@@ -13,6 +13,8 @@ import {
   GridSortItem,
 } from '@mui/x-data-grid-pro';
 import * as React from 'react';
+import { Footer } from './footer';
+
 const columns: GridColDef[] = [
   {
     align: 'left',
@@ -150,52 +152,7 @@ const DatatableItems = () => {
         checkboxSelection={true}
         components={{
           Footer: () => {
-            return (
-              <Box className="sub_pagination_wrapper" component="div">
-                <Box component="div" className="sub_pagination_outer">
-                  <Select
-                    fullWidth={false}
-                    id="btn_batch"
-                    name="btn_batch"
-                    defaultValue="상태 일괄 변경"
-                    className="sub_select_common sub_select_batch"
-                  >
-                    <MenuItem value="상태 일괄 변경">상태 일괄 변경</MenuItem>
-                  </Select>
-                  <Button
-                    variant="contained"
-                    className="sub_btn_primary_fill_common sub_btn_footer_save"
-                  >
-                    변경하기
-                  </Button>
-                </Box>
-                <Box component="div" className="sub_pagination_outer"></Box>
-                <Box
-                  component="div"
-                  className="sub_pagination_outer"
-                  sx={{ justifyContent: 'end' }}
-                >
-                  <Button
-                    variant="outlined"
-                    className="sub_btn_primary_outline_common sub_btn_footer_save"
-                  >
-                    아이템 삭제
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    className="sub_btn_primary_outline_common sub_btn_footer_save"
-                  >
-                    아이템 수정
-                  </Button>
-                  <Button
-                    variant="contained"
-                    className="sub_btn_primary_fill_common sub_btn_footer_save"
-                  >
-                    아이템 등록
-                  </Button>
-                </Box>
-              </Box>
-            );
+            return <Footer />;
           },
         }}
       />
