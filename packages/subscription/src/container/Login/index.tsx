70c855086262afa017966d261e64a1f57680b872
DESKTOP-CMLI6UL\joesu <joe@redcube.pw>
2022-09-01 10:33:19 +0900

    crypto-js

--- packages/subscription/src/container/Login/index.tsx
+++ packages/subscription/src/container/Login/index.tsx
@@ -17,7 +17,7 @@ import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 import { Axios } from '../../utils/axios';
 import Image from '../../../public/assets/images/admin_bkg.png';
-var cryptojs = require('crypto-js');
+import cryptojs from 'crypto-js';
 
 const AdminLogin = () => {
   const [usrId, setUsrId] = React.useState('');
