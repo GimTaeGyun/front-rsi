8148936254b4b31542390e40962d78c63add4abc
James <james@g9soft.com>
2022-09-01 09:55:47 +0900

    crypto-js import

--- packages/subscription/src/components/MenuCommon/admin/index.tsx
+++ packages/subscription/src/components/MenuCommon/admin/index.tsx
@@ -12,7 +12,7 @@ import DataTable from './components/Datatable';
 import ModifySettingsPopup from './components/ModifySettingsPopup';
 import Sidebar, { ITreeItem } from './components/Sidebar';
 import UpdateOperatorPopup from './components/UpdateOperatorPopup';
-import cryptojs from 'crypto-js';
+var cryptojs = require('crypto-js');
 
 const defaultOperPopupData = {
   action: 'add',
